95896101ef38c4fd6209293f68ab0fe0cb6d6cd5
Aditya Pant <adityarajpant@gmail.com>
2019-11-03 14:51:27 +0530

    Added basic .zshrc

--- init.sh
+++ init.sh
@@ -6,10 +6,12 @@ DIR=$(cd `dirname $0` && pwd)
 # Symlink to the home, overwrite
 ln -sf $DIR/.tmux.conf ~/.tmux.conf
 ln -sf $DIR/.vimrc ~/.vimrc
+ln -sf $DIR/.zshrc ~/.zshrc
 
 # Source
+. ~/.zshrc
 
 # Include local file if exists
-# if [ -f ~/.zshrc.local ]; then
-	# source ~/.zshrc.local
-# fi
+if [ -f ~/.zshrc.local ]; then
+	source ~/.zshrc.local
+fi
